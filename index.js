613a8f6aa4e58b17c2e5eafa90800cab4d398b23
arnorhs <arnorhs@gmail.com>
2013-05-14 03:35:29 +0000

    Simplified the code to flatten

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 module.exports = flatten;
 
 function flatten(list){
-  var result;
-
-  if(!Array.isArray(list)) return list;
-
-  result = [];
-
-  list.forEach(function(el){
-
-    if(!Array.isArray(el)) {
-       return result.push(el);
-    }
-
-    result.push.apply(result, flatten(el));
-
-  });
-
-  return result;
+  return list.reduce(function(prev, curr){
+    return prev.concat(Array.isArray(curr) ? flatten(curr) : curr);
+  }, []);
 }
